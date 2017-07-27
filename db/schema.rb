08ac11dca18c2d082bcda7f688878f4b0741fb38
Martine Gagne <martine.e.gagne@gmail.com>
2017-07-26 21:12:59 -0400

    changed table name PartyGuests to widget

--- db/schema.rb
+++ db/schema.rb
@@ -10,19 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170727005757) do
-
-  create_table "PartyGuests", force: :cascade do |t|
-    t.string  "first_name"
-    t.string  "last_name"
-    t.text    "dietary_restrictions"
-    t.integer "salary"
-    t.integer "number_of_kids"
-    t.string  "vulnerabilities"
-    t.string  "illnesses"
-    t.string  "medication"
-    t.string  "voting_preferences"
-  end
+ActiveRecord::Schema.define(version: 20170727010843) do
 
   create_table "company_locations", force: :cascade do |t|
     t.string "address"
@@ -35,6 +23,18 @@ ActiveRecord::Schema.define(version: 20170727005757) do
     t.string  "manufacturer"
     t.decimal "cost"
     t.integer "quantity"
+  end
+
+  create_table "widgets", force: :cascade do |t|
+    t.string  "first_name"
+    t.string  "last_name"
+    t.text    "dietary_restrictions"
+    t.integer "salary"
+    t.integer "number_of_kids"
+    t.string  "vulnerabilities"
+    t.string  "illnesses"
+    t.string  "medication"
+    t.string  "voting_preferences"
   end
 
 end
